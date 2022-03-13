69c233d8b531602d43210db03efc0adb6be85f8e
Miguel Souto <miguel.alejandro.souto@gmail.com>
2022-03-13 00:14:03 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './Components/Header'
+import Form from './Components/Form'
+import PatientList from './Components/PatientList'
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Form/>
+      <PatientList/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/PatientList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PatientList(){
+    return(
+        <h2>Listado de pacientes</h2>
+    )
+}
+
+export default PatientList
