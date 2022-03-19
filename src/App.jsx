529cf328cce25433a4f6e2b8b57a39372d0f0f9a
Miguel Souto <miguel.alejandro.souto@gmail.com>
2022-03-19 14:26:01 -0300

    Se modifica a create-react-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,8 @@
-import Header from './Components/Header'
-import Form from './Components/Form'
-import PatientList from './Components/PatientList'
-import { useState } from 'react'
 
 function App() {
-
-  const [ patients, setPatients ] = useState([])
-  const [ patient, setPatient ] = useState({})
-
-  console.log(patient)
-
   return (
     <>
-    <div className = "container mx-auto bg-slate-100 h-screen">
-      <Header/>
 
-      <Form
-        patients = {patients}
-        setPatients = {setPatients}
-        patient = {patient}
-      />
-
-      <PatientList
-        patients = {patients}
-        setPatient = {setPatient}
-      />
-    </div>
     </>
   )
 }
